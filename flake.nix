267b0496edab745d43156bdd792dba12e4bdfa0c
Nick Cao <nickcao@nichi.co>
2023-06-22 14:35:40 +0800

    build appimage

--- flake.nix
+++ flake.nix
@@ -2,26 +2,40 @@
   inputs = {
     nixpkgs.url = "github:NickCao/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-appimage = {
+      url = "github:ralismark/nix-appimage";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-appimage }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = import nixpkgs { inherit system; }; in with pkgs; {
-          packages.default = with python3Packages; buildPythonApplication {
-            name = "ruyi-ng";
-            src = self;
+          packages = rec {
+            default = with python3Packages; buildPythonApplication {
+              name = "ruyi-ng";
+              src = self;
 
-            format = "pyproject";
+              format = "pyproject";
 
-            nativeBuildInputs = [ poetry-core ];
-            propagatedBuildInputs = [ click ];
+              nativeBuildInputs = [ poetry-core ];
+              propagatedBuildInputs = [ click ];
 
-            makeWrapperArgs = [
-              "--prefix"
-              "PATH"
-              ":"
-              (lib.makeBinPath [ bubblewrap ostree ostree-rs-ext ])
-            ];
+              makeWrapperArgs = [
+                "--prefix"
+                "PATH"
+                ":"
+                (lib.makeBinPath [ bubblewrap ostree ostree-rs-ext ])
+              ];
+
+              meta.mainProgram = "ruyi";
+            };
+
+            appimage = nix-appimage.mkappimage.${system} {
+              drv = default;
+              entrypoint = lib.getExe default;
+              name = default.name;
+            };
           };
         }
       );
